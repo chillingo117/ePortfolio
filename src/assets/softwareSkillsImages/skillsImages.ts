14676e75f58c8bce2ea8ecea0d24271967f682fa
William Chen <kiwitwwilliam@gmail.com>
2025-05-14 14:51:41 +1000

    images for skills tokens

---
+++ src/assets/softwareSkillsImages/skillsImages.ts
@@ -0,0 +1,29 @@
+import tsImg from "./ts.png";
+import cImg from "./c_sharp.png";
+import netImg from "./net.png";
+import scikitImg from "./scikit.png";
+import jupyterImg from "./jupyter.png";
+import lambdaImg from "./lambda.png";
+import axiosImg from "./axios.png";
+import gqlImg from "./gql.png";
+import sqlServerImg from "./sqlServer.png";
+import pgImg from "./pg.png";
+import js from "./js.png";
+import react from "./react.png";
+import python from "./python.png";
+
+export default {
+    ts: tsImg,
+    js: js,
+    react: react,
+    c: cImg,
+    net: netImg,
+    python: python,
+    scikit: scikitImg,
+    jupyter: jupyterImg,
+    lambda: lambdaImg,
+    axios: axiosImg,
+    gql: gqlImg,
+    sqlServer: sqlServerImg,
+    pg: pgImg,
+}
